0145fc92ccc251dc12272596ce846b5c499cc5b9
ofek <ofek.ohayon41@gmail.com>
2022-02-21 10:26:12 +0200

    add log file

--- src/log.py
+++ src/log.py
@@ -1,7 +1,10 @@
 import logging
+from agent import create_folder
 
 
 def config_logger():
+    create_folder('./logs')
     FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
-    logging.basicConfig(format=FORMAT)
+
+    logging.basicConfig(filename=f'./logs/try.log', format=FORMAT)
     return logging.getLogger()
